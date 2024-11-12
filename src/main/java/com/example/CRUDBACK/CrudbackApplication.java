494fe614576fab2ea5060eb463f2c897018048b0
AlexeiMH <L20021260@veracruz.tecnm.mx>
2024-11-12 13:35:57 -0600

    Añadi comentarios sobre cambios para conexion al servidor

--- src/main/java/com/example/CRUDBACK/CrudbackApplication.java
+++ src/main/java/com/example/CRUDBACK/CrudbackApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudbackApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(CrudbackApplication.class, args);
+
 	}
 
 }
