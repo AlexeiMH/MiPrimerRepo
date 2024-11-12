9ed60ab27e4656f40fdd541ebf2769a8523b48f9
JonathanArteaga01 <l21020122@veracruz.tecnm.mx>
2024-11-12 14:08:21 -0600

    agrege getter y setter en Usuario.java

--- src/main/java/com/example/CRUDBACK/Entity/Usuario.java
+++ src/main/java/com/example/CRUDBACK/Entity/Usuario.java
@@ -18,4 +18,13 @@ public class Usuario {
 
     private String password;
 
+    // Getters y Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getEmail() { return correo; }
+    public void setEmail(String email) { this.correo = email; }
+
 }
+
